e51179886f53791b60c3d9eb4b383fc97cbdd726
Santiago Comeras Oteo <santiagocomerasoteo@gmail.com>
2024-05-04 00:47:56 +0200

    ADD - Routes consts

--- src/controller-routes/calculator.routes.ts
+++ src/controller-routes/calculator.routes.ts
@@ -1,9 +1,19 @@
 import { IFileFunction, TYPES } from "sco-backend-fw";
 
+export const CALCULATOR_ROUTES_PATH: string = 'calculator';
+
+export const CALCULATOR_ROUTES_NAMES = {
+    ADD: 'add',
+    SUBTRACT: 'subtract',
+    MULTIPLY: 'multiply',
+    SPLIT: 'split',
+    REST: 'rest',
+};
+
 export const CALCULATOR_ROUTES: IFileFunction[] = [
     {
-        file: 'add',
-        path: 'calculator',
+        file: CALCULATOR_ROUTES_NAMES.ADD,
+        path: CALCULATOR_ROUTES_PATH,
         params: [
             { name: 'n1', type: TYPES.NUMBER, optional: false },
             { name: 'n2', type: TYPES.NUMBER, optional: false },
@@ -11,8 +21,8 @@ export const CALCULATOR_ROUTES: IFileFunction[] = [
         resultType: TYPES.NUMBER,
     },
     {
-        file: 'subtract',
-        path: 'calculator',
+        file: CALCULATOR_ROUTES_NAMES.SUBTRACT,
+        path: CALCULATOR_ROUTES_PATH,
         params: [
             { name: 'n1', type: TYPES.NUMBER, optional: false },
             { name: 'n2', type: TYPES.NUMBER, optional: false },
@@ -20,8 +30,8 @@ export const CALCULATOR_ROUTES: IFileFunction[] = [
         resultType: TYPES.NUMBER,
     },
     {
-        file: 'multiply',
-        path: 'calculator',
+        file: CALCULATOR_ROUTES_NAMES.MULTIPLY,
+        path: CALCULATOR_ROUTES_PATH,
         params: [
             { name: 'n1', type: TYPES.NUMBER, optional: false },
             { name: 'n2', type: TYPES.NUMBER, optional: false },
@@ -29,8 +39,8 @@ export const CALCULATOR_ROUTES: IFileFunction[] = [
         resultType: TYPES.NUMBER,
     },
     {
-        file: 'split',
-        path: 'calculator',
+        file: CALCULATOR_ROUTES_NAMES.SPLIT,
+        path: CALCULATOR_ROUTES_PATH,
         params: [
             { name: 'n1', type: TYPES.NUMBER, optional: false },
             { name: 'n2', type: TYPES.NUMBER, optional: false },
@@ -38,8 +48,8 @@ export const CALCULATOR_ROUTES: IFileFunction[] = [
         resultType: TYPES.NUMBER,
     },
     {
-        file: 'rest',
-        path: 'calculator',
+        file: CALCULATOR_ROUTES_NAMES.REST,
+        path: CALCULATOR_ROUTES_PATH,
         params: [
             { name: 'n1', type: TYPES.NUMBER, optional: false },
             { name: 'n2', type: TYPES.NUMBER, optional: false },
--- src/controller-routes/users.routes.ts
+++ src/controller-routes/users.routes.ts
@@ -1,26 +1,35 @@
 import { IFileFunction, TYPES } from "sco-backend-fw";
 import { UserDto } from "../modules/users/dto/user.dto";
 
+export const USERS_ROUTES_PATH: string = 'users';
+
+export const USERS_ROUTES_NAMES = {
+    FIND: 'find',
+    ADD: 'add',
+    UPDATE: 'update',
+    DELETE: 'delete',
+};
+
 export const USERS_ROUTES: IFileFunction[] = [
     {
-        file: 'find',
-        path: 'users',
+        file: USERS_ROUTES_NAMES.FIND,
+        path: USERS_ROUTES_PATH,
         params: [
             { name: 'query', type: TYPES.ANY, optional: true }
         ],
         resultType: TYPES.ARRAY,
     },
     {
-        file: 'add',
-        path: 'users',
+        file: USERS_ROUTES_NAMES.ADD,
+        path: USERS_ROUTES_PATH,
         params: [
             { name: 'user', type: TYPES.OBJECT, optional: false, dto: UserDto }
         ],
         resultType: TYPES.MODEL,
     },
     {
-        file: 'update',
-        path: 'users',
+        file: USERS_ROUTES_NAMES.UPDATE,
+        path: USERS_ROUTES_PATH,
         params: [
             { name: '_id', type: TYPES.STRING, optional: false },
             { name: 'user', type: TYPES.OBJECT, optional: false, dto: UserDto }
@@ -28,8 +37,8 @@ export const USERS_ROUTES: IFileFunction[] = [
         resultType: TYPES.MODEL,
     },
     {
-        file: 'delete',
-        path: 'users',
+        file: USERS_ROUTES_NAMES.DELETE,
+        path: USERS_ROUTES_PATH,
         params: [
             { name: '_id', type: TYPES.STRING, optional: false },
         ],
