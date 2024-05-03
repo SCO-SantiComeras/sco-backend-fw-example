bbab625cd6eb838f487408cbaea3d08aeff539e1
Santiago Comeras Oteo <santiagocomerasoteo@gmail.com>
2024-05-03 21:23:20 +0200

    v1

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,189 +1,40 @@
 import { Injectable } from "@nestjs/common";
-import { ICore, IFileFunction, TYPES } from "sco-backend-fw";
+import { FileFunctionsService, ICore, IFileFunction, TYPES } from "sco-backend-fw";
 import { WebsocketsService } from "./core/websockets/websockets.service";
 import { MongoDbService } from "./core/mongo-db/mongo-db.service";
 import { HttpErrorsService } from "./core/shared/http-errors/http-errors.service";
 import { ValidationErrorsService } from "./core/shared/validation-errors/validation-errors.service";
 import { UsersService } from "./core/users/users.service";
-import { UserDto } from "./core/users/dto/user.dto";
+import { CALCULATOR_ROUTES } from "./controller-routes/calculator.routes";
+import { USERS_ROUTES } from "./controller-routes/users.routes";
+import { GLOBAL_ROUTES } from "./controller-routes/global.routes";
 
 @Injectable()
 export class AppService implements ICore {
 
     /* Add Own Dependencies */
     constructor(
+        private readonly fileFunctionsService: FileFunctionsService,
         private readonly websocketsService: WebsocketsService,
         private readonly mongodbService: MongoDbService,
         private readonly httpErrorsService: HttpErrorsService,
         private readonly validationErrorsService: ValidationErrorsService,
         private readonly usersService: UsersService,
-    ) {}
+    ) {
+        this.fileFunctionsService.setFileFunctions(this.getFileFunctionsConstants());
+    }
 
-    /*  Function Files Constants*/
-    getFuntionFilesConstants(): IFileFunction[] {
+    /* Function Files Constants */
+    getFileFunctionsConstants(): IFileFunction[] {
         return [
             /* Global */
-            {
-                file: 'hello',
-                path: 'global',
-                resultType: 'string',
-            },
+            ...GLOBAL_ROUTES,
 
             /* Calculator */
-            ...this.createCalculatorFileConstants(),
+            ...CALCULATOR_ROUTES,
         
             /* Users */
-            ...this.createUsersFileConstants(),
-        ];
-    }
-
-    private createCalculatorFileConstants(): IFileFunction[] {
-        return [
-            {
-                file: 'add',
-                path: 'calculator',
-                params: [
-                    {
-                        name: 'n1',
-                        type: 'number',
-                        optional: false,
-                    },
-                    {
-                        name: 'n2',
-                        type: 'number',
-                        optional: false,
-                    },
-                ],
-                resultType: 'number',
-            },
-            {
-                file: 'subtract',
-                path: 'calculator',
-                params: [
-                    {
-                        name: 'n1',
-                        type: 'number',
-                        optional: false,
-                    },
-                    {
-                        name: 'n2',
-                        type: 'number',
-                        optional: false,
-                    },
-                ],
-                resultType: 'number',
-            },
-            {
-                file: 'multiply',
-                path: 'calculator',
-                params: [
-                    {
-                        name: 'n1',
-                        type: 'number',
-                        optional: false,
-                    },
-                    {
-                        name: 'n2',
-                        type: 'number',
-                        optional: false,
-                    },
-                ],
-                resultType: 'number',
-            },
-            {
-                file: 'split',
-                path: 'calculator',
-                params: [
-                    {
-                        name: 'n1',
-                        type: 'number',
-                        optional: false,
-                    },
-                    {
-                        name: 'n2',
-                        type: 'number',
-                        optional: false,
-                    },
-                ],
-                resultType: 'number',
-            },
-            {
-                file: 'rest',
-                path: 'calculator',
-                params: [
-                    {
-                        name: 'n1',
-                        type: 'number',
-                        optional: false,
-                    },
-                    {
-                        name: 'n2',
-                        type: 'number',
-                        optional: false,
-                    },
-                ],
-                resultType: 'number',
-            },
-        ];
-    }
-
-    private createUsersFileConstants(): IFileFunction[] {
-        return [
-            {
-                file: 'find',
-                path: 'users',
-                params: [
-                    {
-                        name: 'query',
-                        type: 'any',
-                        optional: true,
-                    }
-                ],
-                resultType: 'array',
-            },
-            {
-                file: 'add',
-                path: 'users',
-                params: [
-                    {
-                        name: 'user',
-                        type: 'object',
-                        optional: false,
-                        dto: UserDto,
-                    }
-                ],
-                resultType: 'model',
-            },
-            {
-                file: 'update',
-                path: 'users',
-                params: [
-                    {
-                        name: '_id',
-                        type: 'string',
-                        optional: false,
-                    },
-                    {
-                        name: 'user',
-                        type: 'object',
-                        optional: false,
-                        dto: UserDto,
-                    }
-                ],
-                resultType: 'model',
-            },
-            {
-                file: 'delete',
-                path: 'users',
-                params: [
-                    {
-                        name: '_id',
-                        type: 'string',
-                        optional: false,
-                    },
-                ],
-                resultType: 'boolean',
-            },
+            ...USERS_ROUTES,
         ];
     }
 
