aacad9558d09d706817da73edae1bf990aad86fa
Santiago Comeras Oteo <santiagocomerasoteo@gmail.com>
2024-05-03 21:32:41 +0200

    ADD - Shared module import

--- src/core/users/users.module.ts
+++ src/core/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { UsersService } from "./users.service";
+import { SharedModule } from "../shared/shared.module";
 
 @Module({
   imports: [
-
+    SharedModule,
   ],
   providers: [
     UsersService,
