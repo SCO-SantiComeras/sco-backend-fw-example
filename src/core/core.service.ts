2b1df0fd082db5b30d365f17039256efcc882b77
Santiago Comeras Oteo <santiagocomerasoteo@gmail.com>
2024-05-06 16:13:12 +0200

    UPD - setCustomTypes name

--- src/core/core.service.ts
+++ src/core/core.service.ts
@@ -35,7 +35,7 @@ export class CoreService implements ICore {
   }
 
   /* Types */
-  setCustomResultTypes(): any {
+  setCustomTypes(): any {
     return {
       ...TYPES, // Default Types, you are not required to set default types
     }
